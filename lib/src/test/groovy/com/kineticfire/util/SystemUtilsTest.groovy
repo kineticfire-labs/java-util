bfe6e52fb66b1d7d9d962785f0593995eb6965b3
Kris Hall <kjh@ieee.org>
2023-08-14 08:00:18 -0400

    build(lib) Commit gradle-wrapper.jar - Update .gitignore build(lib) Move dependency versions from build.gradle to gradle.properties refactor(lib) Refactor package from com.kineticfire.systems.utis to com.kineticfire.util

--- lib/src/test/groovy/com/kineticfire/util/SystemUtilsTest.groovy
+++ lib/src/test/groovy/com/kineticfire/util/SystemUtilsTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.kineticfire.utils
+package com.kineticfire.util
 
 
 import java.nio.file.Path
